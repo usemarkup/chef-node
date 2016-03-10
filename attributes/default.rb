01ee2ddcd1134757676f9c89cc3956e6e72e8e87
Gavin Staniforth <gavin@usemarkup.com>
2016-03-10 16:14:51 +0000

    fixs(attributes) - allows for 0.10 release to be configured

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default[:node][:version] = "5.7.1-1"
-default[:node][:major_version] = "5"
+default[:node][:major_version] = "5.x"
 
 default[:node][:epel] = node['platform_version'].to_i
 
@@ -13,7 +13,7 @@ end
 default[:node][:host] = "rpm.nodesource.com"
 default[:node][:schema] = "https"
 
-default[:node][:link] = "#{node[:node][:schema]}://#{node[:node][:host]}/pub_#{node[:node][:major_version]}.x/el/#{node[:node][:epel]}/x86_64/#{node[:node][:file]}"
+default[:node][:link] = "#{node[:node][:schema]}://#{node[:node][:host]}/pub_#{node[:node][:major_version]}/el/#{node[:node][:epel]}/x86_64/#{node[:node][:file]}"
 
 # EPEL 6: https://rpm.nodesource.com/pub_5.x/el/6/x86_64/nodejs-5.7.1-1nodesource.el6.x86_64.rpm
 # EPEL 7: https://rpm.nodesource.com/pub_5.x/el/7/x86_64/nodejs-5.7.1-1nodesource.el7.centos.x86_64.rpm
