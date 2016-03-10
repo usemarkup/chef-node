6c89733664ec22e2796d8f7d6e8def4867ec2bf0
Gavin Staniforth <gsdevme@users.noreply.github.com>
2016-03-10 14:59:06 +0000

    fix(metadata) - corrects metadata name

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-name             'chef-node'
+name             'node'
 license          'MIT Licence'
 description      'Installs nodejs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
