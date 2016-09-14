0dbbccc0819418cd7622fe4f1087201a39ec59c8
Gavin Staniforth <gsdevme@users.noreply.github.com>
2016-09-14 10:24:57 +0100

    chore: 1.0.0 release

--- metadata.rb
+++ metadata.rb
@@ -2,4 +2,4 @@ name             'node'
 license          'MIT Licence'
 description      'Installs nodejs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '1.0.0'
