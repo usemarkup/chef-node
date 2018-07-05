0b4e909012fa988326f82a289f140455d9fb79c1
Gavin Staniforth <gavin@usemarkup.com>
2018-07-05 16:16:09 +0100

    chore: release 2.4.0

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'nodesource'
 license 'MIT Licence'
 description 'Installs nodejs from nodesource'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.3.0'
+version          '2.4.0'
 
 maintainer 'Gavin Staniforth'
 maintainer_email 'gavin@usemarkup.com'
